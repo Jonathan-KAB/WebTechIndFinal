5dff7190dafbb4c87981aced787f4f00634fabb4
Jonathan Boateng <144915527+Jonathan-KAB@users.noreply.github.com>
2024-12-18 11:58:43 +0000

    Update database.php
    Removed password from database.php for security reasons

--- Jonathan_WebTech_Final_Deploy/Tasker/config/database.php
+++ Jonathan_WebTech_Final_Deploy/Tasker/config/database.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', 'jonathan.boateng');
-define('DB_PASS', 'jonathan@ashesi');
+define('DB_PASS', 'removed_for_security_reasons');
 define('DB_NAME', 'webtech_fall2024_jonathan_boateng');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
